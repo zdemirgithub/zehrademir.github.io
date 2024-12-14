be66a84dabbc7726f5329b60b2f6a3da3ce43223
zdemirgithub <zehrademir.gm@gmail.com>
2024-12-14 18:46:35 +0300

    Create About.js

---
+++ components/About.js
@@ -0,0 +1,15 @@
+// components/About.js
+const About = () => {
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+      <p>
+        I'm Zehra Demir, a full-stack software engineer passionate about building
+        innovative, scalable systems. I have experience with a variety of technologies including Next.js, React.js, AI, and cloud services.
+        I've worked at leading tech companies and startups, creating apps ranging from e-commerce platforms to autonomous systems.
+      </p>
+    </section>
+  );
+};
+
+export default About;

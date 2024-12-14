96fad4e945e65a4e521b1c44fa49e266d5bb4dd7
zdemirgithub <zehrademir.gm@gmail.com>
2024-12-14 19:07:27 +0300

    Update Sidebar.js

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,17 +1,16 @@
-import React from 'react';
 import Link from 'next/link';
 import styles from '../styles/Sidebar.module.css';
 
 const Sidebar = () => {
   return (
-    <div className={styles.sidebar}>
-      <h2>Quick Links</h2>
+    <aside className={styles.sidebar}>
       <ul>
-        <li><Link href="/projects">All Projects</Link></li>
-        <li><Link href="/about">About Me</Link></li>
-        <li><Link href="/contact">Contact</Link></li>
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/projects">Projects</Link></li>
+        <li><Link href="#about">About</Link></li>
+        <li><Link href="#contact">Contact</Link></li>
       </ul>
-    </div>
+    </aside>
   );
 };
 

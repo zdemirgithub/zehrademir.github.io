468e7e186515ae9fe19373b86ae035c3ea42a3f3
zdemirgithub <zehrademir.gm@gmail.com>
2024-12-14 18:10:31 +0300

    Create Sidebar.js

---
+++ src/components/Sidebar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Sidebar = () => {
+    return (
+        <nav className="sidebar">
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Sidebar;

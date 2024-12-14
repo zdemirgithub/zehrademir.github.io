e858f71ddc8fe9fb0de9d590abb5d78c612d1e0b
zdemirgithub <zehrademir.gm@gmail.com>
2024-12-14 19:07:38 +0300

    Update ProjectCard.js

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,16 +1,14 @@
-// components/ProjectCard.js
-import styles from '../styles/ProjectCard.module.css';
 import Link from 'next/link';
+import styles from '../styles/ProjectCard.module.css';
 
 const ProjectCard = ({ project }) => {
   return (
     <div className={styles.card}>
       <h3>{project.name}</h3>
-      <p><strong>Stack:</strong> {project.stack}</p>
-      <p><strong>Deployment:</strong> {project.deployment}</p>
-      <p>{project.specs}</p>
+      <p>{project.stack}</p>
+      <p>{project.description}</p>
       <Link href={`/projects/${project.id}`}>
-        <a>View Details</a>
+        <a className={styles.cardLink}>View Details</a>
       </Link>
     </div>
   );

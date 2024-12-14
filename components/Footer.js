5f607197c34ad42c05e67d719933d43f6e15b3bd
zdemirgithub <zehrademir.gm@gmail.com>
2024-12-14 18:50:46 +0300

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,9 @@
-// components/Footer.js
-const Footer = () => {
-  return (
-    <footer>
-      <p>© 2024 Zehra Demir. All rights reserved.</p>
-    </footer>
-  );
-};
+const Footer = () => (
+  <footer className="bg-gray-900 text-white py-4">
+    <div className="text-center">
+      <p>&copy; 2024 Zehra Demir. All rights reserved.</p>
+    </div>
+  </footer>
+);
 
 export default Footer;

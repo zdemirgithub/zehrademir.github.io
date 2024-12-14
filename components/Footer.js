3eff03fa4dcfe0dd4edddf1a38c90e5a0d033d9b
zdemirgithub <zehrademir.gm@gmail.com>
2024-12-14 19:08:20 +0300

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,13 +1,7 @@
-import React from 'react';
-import styles from '../styles/Footer.module.css';
-
 const Footer = () => {
   return (
-    <footer className={styles.footer}>
-      <p>Connect with me:</p>
-      <a href="https://github.com/zdemirgithub" target="_blank" rel="noopener noreferrer">GitHub</a>
-      <a href="https://www.linkedin.com/in/zehra-demir-74b307308" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-      <a href="mailto:zehrademir.gm@gmail.com">Email</a>
+    <footer>
+      <p>&copy; 2024 Zehra Demir. All rights reserved.</p>
     </footer>
   );
 };

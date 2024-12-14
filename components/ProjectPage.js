dc7b27bcb625fb6452b98894e2dd64f29abe6b7d
zdemirgithub <zehrademir.gm@gmail.com>
2024-12-14 19:07:50 +0300

    Update ProjectPage.js

--- components/ProjectPage.js
+++ components/ProjectPage.js
@@ -1,6 +1,6 @@
-// components/ProjectPage.js
 import { useRouter } from 'next/router';
 import { projects } from '../data/projects';
+import styles from '../styles/ProjectPage.module.css';
 
 const ProjectPage = () => {
   const router = useRouter();
@@ -12,16 +12,16 @@ const ProjectPage = () => {
   }
 
   return (
-    <div>
+    <div className={styles.projectPage}>
       <h2>{project.name}</h2>
-      <p><strong>Tech Stack:</strong> {project.stack}</p>
+      <p><strong>Stack:</strong> {project.stack}</p>
       <p><strong>Framework:</strong> {project.framework}</p>
       <p><strong>Deployment:</strong> {project.deployment}</p>
       <p><strong>Specs:</strong> {project.specs}</p>
       <p>{project.description}</p>
-      {/* Add dynamic graphs/visualizations here (e.g. using Chart.js or D3.js) */}
     </div>
   );
 };
 
 export default ProjectPage;
+

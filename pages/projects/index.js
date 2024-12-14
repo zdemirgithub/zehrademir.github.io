d8eb5d09a30cb25ce36caeb976596343df37ee1a
zdemirgithub <zehrademir.gm@gmail.com>
2024-12-14 19:01:44 +0300

    Update index.js

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,28 +1,29 @@
-// pages/projects/index.js
-import Header from '../../components/Header';
-import Sidebar from '../../components/Sidebar';
+import React from 'react';
 import { projects } from '../../data/projects';
 import ProjectCard from '../../components/ProjectCard';
-import About from '../../components/About';
-import Contact from '../../components/Contact';
+import Header from '../../components/Header';
+import Sidebar from '../../components/Sidebar';
 import Footer from '../../components/Footer';
 
-export default function Projects() {
+const ProjectsPage = () => {
   return (
     <div>
       <Header />
-      <Sidebar />
-      <main>
-        <h2>My Projects</h2>
-        <div className="project-list">
-          {projects.map((project) => (
-            <ProjectCard key={project.id} project={project} />
-          ))}
+      <div style={{ display: 'flex' }}>
+        <Sidebar />
+        <div style={{ flex: 1 }}>
+          <h2>Projects</h2>
+          <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+            {projects.map((project) => (
+              <ProjectCard key={project.id} project={project} />
+            ))}
+          </div>
         </div>
-        <About />
-        <Contact />
-      </main>
+      </div>
       <Footer />
     </div>
   );
-}
+};
+
+export default ProjectsPage;
+

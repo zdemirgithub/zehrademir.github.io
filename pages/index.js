192ef22920ac6ef22bb46aa4c56f3f898cc70f18
zdemirgithub <zehrademir.gm@gmail.com>
2024-12-14 18:54:26 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,28 @@
-import Header from '../components/Header';
-import About from '../components/About';
-import Contact from '../components/Contact';
-import Footer from '../components/Footer';
-import Chart from '../components/Chart';
+// pages/projects/index.js
+import Header from '../../components/Header';
+import Header from '../../components/Chart';
+import Sidebar from '../../components/Sidebar';
+import { projects } from '../../data/projects';
+import ProjectCard from '../../components/ProjectCard';
+import About from '../../components/About';
+import Contact from '../../components/Contact';
+import Footer from '../../components/Footer';
 
-export default function Home() {
+export default function Projects() {
   return (
-    <div className="font-sans text-white">
-      {/* Header */}
+    <div>
       <Header />
-
-      {/* About Section */}
-      <About />
-
-      {/* Chart Section */}
-      <Chart />
-
-      {/* Contact Section */}
-      <Contact />
-
-      {/* Footer */}
+      <Sidebar />
+      <main>
+        <h2>My Projects</h2>
+        <div className="project-list">
+          {projects.map((project) => (
+            <ProjectCard key={project.id} project={project} />
+          ))}
+        </div>
+        <About />
+        <Contact />
+      </main>
       <Footer />
     </div>
   );

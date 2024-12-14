73bf10d0cdc733650faba02e256c4f3df50f3350
zdemirgithub <zehrademir.gm@gmail.com>
2024-12-14 18:56:33 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,22 +8,20 @@ import About from '../../components/About';
 import Contact from '../../components/Contact';
 import Footer from '../../components/Footer';
 
-export default function Projects() {
+export default function Home() {
   return (
     <div>
       <Header />
       <Sidebar />
       <main>
-        <h2>My Projects</h2>
+        <h2>Projects</h2>
         <div className="project-list">
           {projects.map((project) => (
             <ProjectCard key={project.id} project={project} />
           ))}
         </div>
-        <About />
-        <Contact />
       </main>
-      <Footer />
     </div>
   );
 }
+
